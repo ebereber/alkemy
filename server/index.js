b73bff249f0d3a4a061f84279393991802f390ec
Eber Leguizamon <eberlegza@gmail.com>
2022-07-25 15:36:47 -0300

    feat:add routes

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const userRouter = require("./routes/user")
+const movementsRouter = require("./routes/movements")
+
+const app = express()
+
+
+app.use("/user",userRouter)
+app.use("/movements",movementsRouter)
+
+
+const PORT = 4001
+app.listen(PORT, ()=> {
+    console.log(`🚀 Server listening on PORT ${PORT} 🚀`)})
---
+++ server/routes/user.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const router = express.Router()
+
+router.post("/signup", (req,res) => {
+
+})
+router.post("/signin", (req,res) => {
+
+})
+router.put("/update", (req,res) => {
+
+})
+router.delete("/delete", (req,res) => {
+
+})
+
+module.exports = router

297398b3c7943c91e8c1cf8ec3bafb0102f49b3a
Eber Leguizamon <eberlegza@gmail.com>
2022-07-26 11:46:21 -0300

    feat: add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import AppRouter from './routes/AppRouter'
 
 function App() {
   return (
-    <></>
+    <ChakraProvider>
+      <AppRouter />
+    </ChakraProvider>
   )
 }
 

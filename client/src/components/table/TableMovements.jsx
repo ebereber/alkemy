95b0554c4d1cc563dbf78c738c6332363b6db0b2
Eber Leguizamon <eberlegza@gmail.com>
2022-07-26 12:03:30 -0300

    feat: add table movements component

---
+++ client/src/components/table/TableMovements.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Box, Table, Th, Thead, Tr } from '@chakra-ui/react'
+
+function TableMovements() {
+  return (
+    <Box w="full" p={50}>
+      <Table size="sm">
+        <Thead>
+          <Tr display={{ base: 'none', md: 'contents' }}>
+            <Th>Date</Th>
+            <Th>Category</Th>
+            <Th>Concep</Th>
+            <Th>Amount</Th>
+            <Th>Action</Th>
+          </Tr>
+          <Tr display={{ base: 'block', md: 'none' }}>
+            <Th>Movements</Th>
+          </Tr>
+        </Thead>
+      </Table>
+    </Box>
+  )
+}
+
+export default TableMovements

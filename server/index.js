f2d9d9ef88ba5018d339822d2d0c765650bbec3e
Eber Leguizamon <eberlegza@gmail.com>
2022-07-26 14:05:47 -0300

    add core

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const userRouter = require('./routes/user')
 const movementsRouter = require('./routes/movements')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/user', userRouter)
 app.use('/movements', movementsRouter)

d355f74d985b89ec5d35dcecf2ba789969fd3488
Eber Leguizamon <eberlegza@gmail.com>
2022-07-26 13:51:14 -0300

    feat: add auth context

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,68 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const AuthContext = createContext()
+
+export const useAuth = () => {
+  return useContext(AuthContext)
+}
+
+const userLocalStorage = JSON.parse(localStorage.getItem('user') || 'null')
+
+axios.interceptors.request.use(
+  (config) => {
+    config.headers.authorization = `Bearer ${userLocalStorage}`
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(userLocalStorage)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  console.log(user)
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(user))
+  }, [user])
+
+  const loggin = (url, user) => {
+    setLoading(true)
+    axios
+      .post(url, user)
+      .then((res) => {
+        const credentials = res.data
+        setUser(credentials.token)
+        setError(null)
+        setLoading(false)
+        navigate('/')
+      })
+      .catch((err) => {
+        console.log(err)
+        const { error } = err.response.data
+        setError(error)
+        setLoading(false)
+        hideError()
+      })
+  }
+
+  const hideError = () => {
+    setTimeout(() => {
+      setError(false)
+    }, 3000)
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, error, loading, loggin, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider

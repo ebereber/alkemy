30a3182298f60f33c0060ca9dbc49c87969097a1
Eber Leguizamon <eberlegza@gmail.com>
2022-07-26 16:36:49 -0300

    feat: add handle logout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,92 +21,107 @@ import TableMovements from '../components/table/TableMovements'
 import { ModalMovement } from '../components/modal/ModalMovement'
 import { useBalance } from '../context/BalanceContext'
 import { FiTrendingUp, FiTrendingDown } from 'react-icons/fi'
+import { useAuth } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
   const { balance } = useBalance()
-  return (
-    <>
-      <Box float={'right'} padding="1rem">
-        <Menu position="relative">
-          <MenuButton as={Button} rounded={'full'} variant={'link'} cursor={'pointer'} minW={0}>
-            <Avatar
-              size={'sm'}
-              src={
-                'https://cdns.iconmonstr.com/wp-content/releases/preview/2016/240/iconmonstr-generation-16.png'
-              }
-            />
-          </MenuButton>
-          <MenuList alignItems={'center'}>
-            <br />
-            <Center>
+  const { user, setUser } = useAuth()
+
+  const navigate = useNavigate()
+
+  const handleLoggout = () => {
+    setUser(null)
+    window.localStorage.removeItem('user')
+    navigate('/signin', { replace: true })
+  }
+
+  if (user) {
+    return (
+      <>
+        <Box float={'right'} padding="1rem">
+          <Menu position="relative">
+            <MenuButton as={Button} rounded={'full'} variant={'link'} cursor={'pointer'} minW={0}>
               <Avatar
-                size={'2xl'}
+                size={'sm'}
                 src={
                   'https://cdns.iconmonstr.com/wp-content/releases/preview/2016/240/iconmonstr-generation-16.png'
                 }
               />
-            </Center>
-            <br />
-            <Center>
-              <p>Username</p>
-            </Center>
-            <br />
-            <MenuDivider />
-            <MenuItem>Your Servers</MenuItem>
-            <MenuItem>Account Settings</MenuItem>
-            <MenuItem>Logout</MenuItem>
-          </MenuList>
-        </Menu>
-      </Box>
+            </MenuButton>
+            <MenuList alignItems={'center'}>
+              <br />
+              <Center>
+                <Avatar
+                  size={'2xl'}
+                  src={
+                    'https://cdns.iconmonstr.com/wp-content/releases/preview/2016/240/iconmonstr-generation-16.png'
+                  }
+                />
+              </Center>
+              <br />
+              <Center>
+                <p>Username</p>
+              </Center>
+              <br />
+              <MenuDivider />
+              <MenuItem>Your Servers</MenuItem>
+              <MenuItem>Account Settings</MenuItem>
+              <MenuItem onClick={() => handleLoggout()}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
 
-      <Center minH={'100vh'}>
-        <VStack w={'100%'}>
-          <Flex justify={'center'}>
-            <Avatar
-              size={'xl'}
-              src={
-                'https://cdns.iconmonstr.com/wp-content/releases/preview/2016/240/iconmonstr-generation-16.png'
-              }
-              alt={'Author'}
-              css={{
-                border: '2px solid white'
-              }}
-            />
-          </Flex>
-          <Heading>Hi ! Username </Heading>
-          <Text>Your balance today</Text>
-          {balance === 0 ? (
-            <Text>Add a movement to start </Text>
-          ) : (
-            <>
-              {' '}
-              <Box borderRadius=".5rem">
-                {balance < 0 ? (
-                  <Icon as={FiTrendingDown} fontSize="2.5rem" color={'black'} />
-                ) : (
-                  <Icon as={FiTrendingUp} fontSize="2.5rem" color={'black'} />
-                )}
-              </Box>
-              <Stack
-                h="100px"
-                w="250px"
-                justify={'center'}
-                align="center"
-                borderRadius=".5rem"
-                boxShadow={'base'}>
-                <HStack>
-                  <Heading>${balance}</Heading>
-                </HStack>
-              </Stack>
-            </>
-          )}
+        <Center minH={'100vh'}>
+          <VStack w={'100%'}>
+            <Flex justify={'center'}>
+              <Avatar
+                size={'xl'}
+                src={
+                  'https://cdns.iconmonstr.com/wp-content/releases/preview/2016/240/iconmonstr-generation-16.png'
+                }
+                alt={'Author'}
+                css={{
+                  border: '2px solid white'
+                }}
+              />
+            </Flex>
+            <Heading>Hi ! Username </Heading>
+            <Text>Your balance today</Text>
+            {balance === 0 ? (
+              <Text>Add a movement to start </Text>
+            ) : (
+              <>
+                {' '}
+                <Box borderRadius=".5rem">
+                  {balance < 0 ? (
+                    <Icon as={FiTrendingDown} fontSize="2.5rem" color={'black'} />
+                  ) : (
+                    <Icon as={FiTrendingUp} fontSize="2.5rem" color={'black'} />
+                  )}
+                </Box>
+                <Stack
+                  h="100px"
+                  w="250px"
+                  justify={'center'}
+                  align="center"
+                  borderRadius=".5rem"
+                  boxShadow={'base'}
+                >
+                  <HStack>
+                    <Heading>${balance}</Heading>
+                  </HStack>
+                </Stack>
+              </>
+            )}
 
-          <ModalMovement />
-          <TableMovements />
-        </VStack>
-      </Center>
-    </>
-  )
+            <ModalMovement />
+            <TableMovements />
+          </VStack>
+        </Center>
+      </>
+    )
+  }
 }
 
 export default Home

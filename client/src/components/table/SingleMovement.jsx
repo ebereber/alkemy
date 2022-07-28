ede2ca6ee951c58b983b92458c4e7e64db770e4a
Eber Leguizamon <eberlegza@gmail.com>
2022-07-28 10:07:51 -0300

    feat: add responsive mobile table and styles

--- client/src/components/table/SingleMovement.jsx
+++ client/src/components/table/SingleMovement.jsx
@@ -1,35 +1,81 @@
-import { Badge, Button, Icon, Tbody, Td, Tr } from '@chakra-ui/react'
+import { Badge, Button, HStack, Icon, Tbody, Td, Text, Tr, VStack } from '@chakra-ui/react'
 
 import { MdDeleteOutline } from 'react-icons/md'
 import React from 'react'
 import { ModalEdit } from '../modal/ModalEdit'
+import { getColor } from '../../utils/getColor'
 
 function SignleMovement({ deleteMovement, movement }) {
   const { id, description, date, amount, category, type } = movement
+
   return (
-    <Tbody
-      _hover={{ bg: 'gray.800', color: 'white' }}
-      display={{ base: 'none', md: 'table-header-group' }}>
-      <Tr>
-        <Td>{date}</Td>
-        <Td>
-          <Badge colorScheme="purple">{category}</Badge>
-        </Td>
-        <Td>{description}</Td>
-        <Td color={type !== 'expense' ? 'green' : 'red'}>${amount}</Td>
-        <Td>
-          <Button
-            bg="gray.600"
-            mr={'1rem'}
-            mb={{ base: '1rem', md: '0' }}
-            _hover={{ bg: 'gray.500' }}
-            onClick={() => deleteMovement(id)}>
-            <Icon color={'white'} as={MdDeleteOutline} />
-          </Button>
-          <ModalEdit movement={movement} />
-        </Td>
-      </Tr>
-    </Tbody>
+    <>
+      {/*  MD */}
+      <Tbody
+        _hover={{ bg: 'gray.800', color: 'white' }}
+        display={{ base: 'none', sm: 'none', md: 'table-header-group' }}
+      >
+        <Tr>
+          <Td>{date}</Td>
+          <Td>
+            <Badge colorScheme={getColor(category)}>{category}</Badge>
+          </Td>
+          <Td textTransform={'capitalize'} fontWeight={'semibold'}>
+            {description}
+          </Td>
+          <Td fontWeight={'medium'} color={type !== 'expense' ? 'green' : 'red'}>
+            ${amount}
+          </Td>
+          <Td>
+            <HStack>
+              <Button
+                bg="gray.600"
+                mr={'1rem'}
+                _hover={{ bg: 'gray.500' }}
+                onClick={() => deleteMovement(id)}
+              >
+                <Icon color={'white'} as={MdDeleteOutline} />
+              </Button>
+              <ModalEdit movement={movement} />
+            </HStack>
+          </Td>
+        </Tr>
+      </Tbody>
+
+      <Tbody
+        _hover={{ bg: 'gray.800', color: 'white' }}
+        display={{ base: 'table-header-group', md: 'none' }}
+      >
+        <Tr>
+          <Td>
+            <HStack>
+              <VStack alignItems={'baseline'}>
+                <Text textTransform={'capitalize'} fontWeight={'medium'}>
+                  {description}
+                </Text>
+                <Text fontWeight={'medium'} color={type !== 'expense' ? 'green' : 'red'}>
+                  ${amount}
+                </Text>
+                <Badge colorScheme={getColor(category)}>{category}</Badge>
+                <Text>{date}</Text>
+              </VStack>
+            </HStack>
+          </Td>
+
+          <Td p="0">
+            <Button
+              bg="gray.600"
+              mr={'1rem'}
+              _hover={{ bg: 'gray.500' }}
+              onClick={() => deleteMovement(id)}
+            >
+              <Icon color={'white'} as={MdDeleteOutline} />
+            </Button>
+            <ModalEdit movement={movement} mb={{ base: '1rem' }} />
+          </Td>
+        </Tr>
+      </Tbody>
+    </>
   )
 }
 
--- client/src/components/table/TableMovements.jsx
+++ client/src/components/table/TableMovements.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box, Table, Th, Thead, Tr } from '@chakra-ui/react'
 import SignleMovement from './SingleMovement'
 import { useBalance } from '../../context/BalanceContext'
@@ -7,16 +7,17 @@ function TableMovements() {
   const { movements, deleteMovement, editMovement } = useBalance()
 
   return (
-    <Box w="full" p={50}>
+    <Box w="full" p={{ base: '0', sm: '1rem', md: '2rem', lg: '6rem' }}>
       <Table size="sm">
         <Thead>
           <Tr display={{ base: 'none', md: 'contents' }}>
             <Th>Date</Th>
             <Th>Category</Th>
-            <Th>Concep</Th>
+            <Th>Concept</Th>
             <Th>Amount</Th>
             <Th>Action</Th>
           </Tr>
+
           <Tr display={{ base: 'block', md: 'none' }}>
             <Th>Movements</Th>
           </Tr>

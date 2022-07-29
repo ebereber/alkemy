e209c2c5ce2c1e7f70bf23aa2038fb9c5a351607
Eber Leguizamon <eberlegza@gmail.com>
2022-07-29 12:29:03 -0300

    feat: add styles to signin signout pages

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,12 +2,15 @@ import {
   Box,
   Button,
   Center,
+  Divider,
   FormControl,
   FormLabel,
   Heading,
   HStack,
+  Image,
   Input,
   LinkBox,
+  Stack,
   Text,
   VStack
 } from '@chakra-ui/react'
@@ -27,67 +30,114 @@ function SignUp() {
   }
 
   return (
-    <Center minH={'100vh'} bg="white" align={'center'}>
-      <VStack>
-        <VStack
-          justifyContent={'center'}
-          borderRadius={'lg'}
-          color={'black'}
-          justify="flex-start"
-          minW={'200px'}>
-          <VStack w={'100%'} align="flex-start">
-            <Heading>M Traker</Heading>
-            <Text>Welcome to Money Tracker!</Text>
-            <HStack fontSize={'12px'}>
-              <Text>Already have an account? </Text>
-              <LinkBox color={'purple.600'} as={Link} to="/signin">
-                Sign in
-              </LinkBox>
+    <Stack
+      flexDirection={{ base: 'column', md: 'row' }}
+      bg="white"
+      align={'center'}
+      justify="space-evenly"
+      minH="100vh"
+    >
+      <VStack
+        p={{ base: 0, sm: '44px 20px', md: '44px 40px' }}
+        m={'46px 0px'}
+        width={{ base: '100%', sm: '540px' }}
+      >
+        <VStack justifyContent={'center'} borderRadius={'lg'} color={'black'} justify="flex-start">
+          <VStack w={'100%'} align="center">
+            <HStack justify={'center'} alignContent="center" pb={'20px'}>
+              <Box boxSize="40px">
+                <Image src="https://cdns.iconmonstr.com/wp-content/releases/preview/2014/240/iconmonstr-cube-18.png" />
+              </Box>
+              <Heading>My balance</Heading>
             </HStack>
+
+            <VStack pb="28px" fontWeight={'400'}>
+              <Text>Welcome to your balance!</Text>
+              <Text></Text>
+            </VStack>
           </VStack>
           <form onSubmit={handleSubmit}>
-            <FormControl>
-              <FormLabel>Username</FormLabel>
-              <Input type="text" onChange={(e) => setUsername(e.target.value)} required />
+            <FormControl pb={'28px'}>
+              <Input
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                variant="flushed"
+                placeholder="Username"
+                focusBorderColor="black"
+              />
+            </FormControl>
+            <FormControl pb={'28px'}>
+              <Input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                variant="flushed"
+                placeholder="Email"
+                focusBorderColor="black"
+              />
             </FormControl>
 
-            <FormControl>
-              <FormLabel>Email</FormLabel>
-              <Input type="email" onChange={(e) => setEmail(e.target.value)} required />
+            <FormControl pb={'28px'}>
+              <Input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                variant="flushed"
+                placeholder="Password"
+                focusBorderColor="black"
+              />
             </FormControl>
-
-            <FormControl>
-              <FormLabel>Password</FormLabel>
-              <Input type="password" onChange={(e) => setPassword(e.target.value)} required />
-            </FormControl>
-            {error && (
-              <Text mt={'1rem'} color={'red.500'} fontSize="12px">
-                {error}
-              </Text>
-            )}
-
-            <Box w={'100%'}>
+            <Box w={'100%'} pb="28px">
+              {error && (
+                <Text mt={'1rem'} color={'red.500'} fontSize="12px">
+                  {error}
+                </Text>
+              )}
               {loading ? (
                 <Button
                   isLoading
-                  loadingText="Sign up"
+                  loadingText="Sign in"
                   type="submit"
                   bg="purple.400"
                   w={'100%'}
                   color={'white'}
-                  mt={6}>
-                  Sign up
+                >
+                  Sign in
                 </Button>
               ) : (
-                <Button type="submit" bg="purple.400" w={'100%'} color={'white'} mt={6}>
-                  Sign up
+                <Button type="submit" bg="black" w={'100%'} color={'white'}>
+                  Sign in
                 </Button>
               )}
             </Box>
+            <HStack fontSize={'13px'}>
+              <Text as={'span'}> Don`t have an account?</Text>
+              <LinkBox
+                textDecoration="underline"
+                color={'black'}
+                fontWeight="bold"
+                as={Link}
+                to="/signin"
+              >
+                Sign in
+              </LinkBox>
+            </HStack>
           </form>
         </VStack>
       </VStack>
-    </Center>
+      <Center height="500px">
+        <Divider orientation="vertical" />
+      </Center>
+      <Box display={{ base: 'none', sm: 'none', md: 'block' }} w="100%">
+        <Box m={'auto'} maxW="540px">
+          <Text as={'span'} fontSize="60px" fontWeight={'700'}>
+            Track your movements and make good decisions.
+          </Text>
+          <Box h={'80px'}></Box>
+        </Box>
+      </Box>
+    </Stack>
   )
 }
 
